927b0bd4a1c217274c78cb17359449822e7004bd
Anas Jahangir <md4909719@gmail.com>
2024-07-04 05:45:12 +0500

    d

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -98,12 +98,12 @@ function Navbar() {
               </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                href="/humanoid"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 BitcoinAI Humanoid
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
